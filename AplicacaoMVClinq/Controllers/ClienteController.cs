41a032a26e79cbb85c039426a240b1f2da31f5d3
ÍtaloSenai2A <italooosenai@gmail.com>
2024-08-27 17:00:15 -0300

    Finalizada a lista de clientes

--- AplicacaoMVClinq/Controllers/ClienteController.cs
+++ AplicacaoMVClinq/Controllers/ClienteController.cs
@@ -7,13 +7,25 @@ namespace AplicacaoMVClinq.Controllers
     {
         public IActionResult Index()
         {
-            new Cliente { Id = 1, Nome = "Alice", Email = "alice@example.com", Telefone = "1234-5678", "2000-12-06" }, 
-     new Cliente { Id = 2, Nome = "Bob", Email = "bob@example.com", Telefone = "2345-6789".  "2001-10-06" }, 
-     new Cliente { Id = 3, Nome = "Charlie", Email = "charlie@example.com", Telefone = "3456-7890", "1999-12-06" }, 
-     new Cliente { Id = 4, Nome = "David", Email = "david@example.com", Telefone = "4567-8901", "2000-12-01" }, 
-     new Cliente { Id = 5, Nome = "Eve", Email = "eve@example.com", Telefone = "5678-9012", "2000-12-06" };
-
             return View();
+        }
+
+        public IActionResult GetCliente()
+        {
+            Cliente c1 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c2 = new Cliente(2, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c3 = new Cliente(3, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c4 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c5 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+
+            List<Cliente> listaClientes = new List<Cliente>();
+            listaClientes.Add(c1);
+            listaClientes.Add(c2);
+            listaClientes.Add(c3);
+            listaClientes.Add(c4);
+            listaClientes.Add(c5);
+
+            return View(listaClientes);
         }
     }
 }
