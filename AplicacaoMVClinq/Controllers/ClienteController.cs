697b3a36b38eb0cf4dcf3b8dfcd2c1c6df843d3f
ÍtaloSenai2A <italooosenai@gmail.com>
2024-08-28 11:06:40 -0300

    Finalizada a atividade de bootstrap

--- AplicacaoMVClinq/Controllers/ClienteController.cs
+++ AplicacaoMVClinq/Controllers/ClienteController.cs
@@ -12,11 +12,11 @@ namespace AplicacaoMVClinq.Controllers
 
         public IActionResult GetCliente()
         {
-            Cliente c1 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
-            Cliente c2 = new Cliente(2, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
-            Cliente c3 = new Cliente(3, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
-            Cliente c4 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
-            Cliente c5 = new Cliente(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            Cliente c1 = new Cliente(1, "Ítalo Francesco", "italo.francesco@gmail.com", "1234-5678", new DateOnly(2008, 3, 28));
+            Cliente c2 = new Cliente(2, "Emanuelly Lima", "manu.lima@gmail.com", "1234-5678", new DateOnly(2008, 1, 25));
+            Cliente c3 = new Cliente(3, "Rayssa Nanclares", "rayssa.nanclares@gmail.com", "1234-5678", new DateOnly(2007, 12, 19));
+            Cliente c4 = new Cliente(4, "Bruno Martins", "bruno.martins@gmail.com", "1234-5678", new DateOnly(2007, 8, 28));
+            Cliente c5 = new Cliente(5, "Alice Gusman", "alice.gusman@gmail.com", "1234-5678", new DateOnly(2000, 6, 1));
 
             List<Cliente> listaClientes = new List<Cliente>();
             listaClientes.Add(c1);
